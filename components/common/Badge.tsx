5a7951bac5d60bfcfab02ff1aa9dc5811f1df4a2
raziel0110 <albangabriel42@gmail.com>
2024-01-22 17:08:11 +0200

    Small styling

--- components/common/Badge.tsx
+++ components/common/Badge.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 interface BadgeUI {
   items: number;
 }
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red', 
+    position:'relative',
+    top: 0,
+    right: 3,
+    height: 14,
+    width: 14,
+    borderRadius: 50
+  },
+  text: {
+    color: 'white',
+    fontSize: 10,
+    textAlign: 'center'
+  },
+});
+
 const Badge = ({items}: BadgeUI ) => {
   return (
-    <View style={{
-      backgroundColor: 'red', 
-      position:'relative',
-      top: 0,
-      right: 3,
-      height: 14,
-      width: 14,
-      borderRadius: 50
-    }}>
-      <Text style={{color: 'white', fontSize: 10, textAlign: 'center'}}>{items}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{items}</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Badge;
