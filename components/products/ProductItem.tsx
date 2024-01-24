8b927847b66821dcb5fd7042e3b9036eae3d69b5
raziel0110 <albangabriel42@gmail.com>
2024-01-24 07:27:23 +0200

    checkout cart small design refactoring

--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { BottomDescription, DescriptionView, ImageContainer, PriceBlock, TitleHeader, ViewContainer } from '../../screens/HomeScreenStyle';
+import { Dimensions } from 'react-native';
+
+const width = Dimensions.get('screen').width;
 
 interface CheckoutCart {
   thumbnail: string;
@@ -9,15 +12,48 @@ interface CheckoutCart {
   price: number;
 }
 
-const ProductItem = ({item}: any): React.JSX.Element => {
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 75,
+    height: 75,
+    borderRadius: 15,
+    marginRight: 24,
+    shadowColor: '#294B29',
+    shadowOpacity: 0.3,
+  },
+  titleHeader: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  itemContainer: {
+    padding: 15,
+    display: 'flex',
+    width: width, 
+    flexDirection: 'row',
+    marginBottom: 10,
+    backgroundColor: 'white',
+    shadowOpacity: 0.2,
+    shadowOffset: {width: 1, height: 7},
+    shadowColor: '#294B29',
+  },
+  descriptionContainer: {
+    marginTop: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  }
+});
+
+const ProductItem = (props: {item: CheckoutCart}): React.JSX.Element => {
+  const {item} = props;
   return (
-    <ViewContainer>
+    <View style={styles.itemContainer}>
       <View>
-        <ImageContainer source={{uri: item.thumbnail}} resizeMode="cover" />
+        <Image source={{uri: item.thumbnail}} resizeMode="cover" style={styles.imageContainer}/>
       </View>
       <View>
-        <TitleHeader>{item.title}</TitleHeader>
-        <DescriptionView>
+        <Text style={styles.titleHeader}>{item.title}</Text>
+        <View style={styles.descriptionContainer}>
           <View>
             <BottomDescription>Stock: {item.stock}</BottomDescription>
           </View>
@@ -26,9 +62,9 @@ const ProductItem = ({item}: any): React.JSX.Element => {
               Price:<PriceBlock>{item.price}$</PriceBlock>
             </Text>
           </View>
-        </DescriptionView>
+        </View>
       </View>
-    </ViewContainer>
+    </View>
   )
 };
 
